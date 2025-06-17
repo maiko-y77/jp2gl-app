59f7a25d837fabdd267fc5e7062fae9a3ad195e7
Maiko Yorino <mk0510@mk@hotmail.co.jp>
2025-06-16 17:29:58 -0700

    画像削除の本番対応構成を戻した

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: "export", // ← 追加！
+  // output: "export", // ← 追加！
   images: {
     unoptimized: true, // ← Cloudinaryやimgタグ対策
   },
